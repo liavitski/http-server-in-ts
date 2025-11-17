b52169e8263a34f0db03a735c7cd901ab3b59c06
Pavel Liavitski <pavel.liavitski@gmail.com>
2025-11-17 16:53:06 +0100

    api config done

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+type APIConfig = {
+  fileserverHits: number;
+};
+
+const config: APIConfig = { fileserverHits: 0 };
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import express from "express";
+import config from "./config.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", middlewareLogResponses, express.static("./src/app"));
+app.use(
+  "/app",
+  middlewareMetricsInc,
+  middlewareLogResponses,
+  express.static("./src/app"),
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
@@ -14,7 +20,19 @@ function handlerReadiness(req: Request, res: Response): void {
   res.set("Content-Type", "text/plain").send("ok");
 }
 
+function handlerRequestCount(req: Request, res: Response): void {
+  const count = config.fileserverHits;
+  res.type("text").send(`Hits: ${count}`); 
+}
+
+function handlerRequestReset(req: Request, res: Response): void {
+  config.fileserverHits = 0;
+  res.type("text").send(`Hits reseted to 0`);
+}
+
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerRequestCount);
+app.get("/reset", handlerRequestReset);
 
 function middlewareLogResponses(
   req: Request,
@@ -22,10 +40,20 @@ function middlewareLogResponses(
   next: NextFunction,
 ): void {
   res.on("finish", () => {
-    if (res.statusCode >= 300) {
+    if (res.statusCode >= 400) {
       console.log(
         `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
       );
+    }
+  });
+
+  next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+  res.on("finish", () => {
+    if (config.fileserverHits !== undefined) {
+      config.fileserverHits++;
     }
   });
 
